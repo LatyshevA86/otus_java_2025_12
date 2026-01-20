9066876a3965fb4e31eeb67ad05c779c1c63d4f5
Латышев Алексей Валерьевич <avlatys3@mts.ru>
2026-01-20 22:29:41 +0300

    save

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -1,21 +1,34 @@
 package homework;
 
-import java.util.Map;
+import static java.util.Comparator.comparingLong;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final NavigableMap<Customer, String> customerMap;
+
+    public CustomerService() {
+        this.customerMap = new TreeMap<>(comparingLong(Customer::getScores));
+    }
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> smallest = customerMap.firstEntry();
+        return Map.entry(deepCopy(smallest.getKey()), smallest.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> next = customerMap.higherEntry(customer);
+        return next != null ? Map.entry(deepCopy(next.getKey()), next.getValue()) : null;
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        customerMap.put(customer, data);
+    }
+
+    private Customer deepCopy(Customer customer) {
+        return new Customer(customer.getId(), customer.getName(), customer.getScores());
+    }
 }
