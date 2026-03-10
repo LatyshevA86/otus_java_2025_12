2a4ecd9578d7aa9b39df0c77a906e8e1caf666b5
Латышев Алексей Валерьевич <avlatys3@mts.ru>
2026-03-10 22:06:29 +0300

    save

--- L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ L15-structuralPatterns/homework/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,26 @@
 package ru.otus.listener.homework;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private final Map<Long, Message> history;
+
+    public HistoryListener() {
+        this.history = new HashMap<>();
+    }
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        history.put(msg.getId(), msg.copy());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(history.get(id)).map(Message::copy);
     }
 }
