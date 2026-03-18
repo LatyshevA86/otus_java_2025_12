6deb9ad9124f9244fa8bb433536d68991ae58ec8
Aleksei Latyshev <82029876+LatyshevA86@users.noreply.github.com>
2026-03-18 19:43:16 +0300

    L16 I/O homework

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,29 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.util.List;
 import ru.otus.model.Measurement;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private final String fileName;
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public List<Measurement> load() {
-        // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        ObjectMapper mapper = new ObjectMapper();
+        try (var inputStream = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (inputStream == null) {
+                throw new FileProcessException("Resource not found: " + fileName);
+            }
+            return mapper.readValue(inputStream, new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
