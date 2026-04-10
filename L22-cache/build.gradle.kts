b6c5598f7c092ff445927848e9b1a693740d763a
Aleksei Latyshev <82029876+LatyshevA86@users.noreply.github.com>
2026-04-10 19:09:10 +0300

    L22 cache homework

--- L22-cache/build.gradle.kts
+++ L22-cache/build.gradle.kts
@@ -1,4 +1,10 @@
 dependencies {
     implementation("ch.qos.logback:logback-classic")
     implementation("org.ehcache:ehcache")
+    implementation("com.zaxxer:HikariCP")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+    implementation("org.postgresql:postgresql")
+    implementation(project(":L18-jdbc:demo"))
+    implementation(project(":L18-jdbc:homework"))
 }
