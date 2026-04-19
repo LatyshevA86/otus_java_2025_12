569a70079e3d247f495d89a7af91bc5a1e8e65b2
Латышев Алексей Валерьевич <avlatys3@mts.ru>
2026-04-19 16:28:21 +0300

    L25 dependency injection homework

--- L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,9 +1,15 @@
 package ru.otus.appcontainer;
 
+import static java.util.Comparator.comparingInt;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
@@ -17,9 +23,65 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        // You code here...
+    public AppComponentsContainerImpl(Class<?>... initialConfigClasses) {
+        processConfig(initialConfigClasses);
+    }
+
+    private void processConfig(Class<?>... configClasses) {
+        Arrays.stream(configClasses)
+            .sorted(comparingInt(config ->
+                config.getAnnotation(AppComponentsContainerConfig.class).order()))
+            .flatMap(configClass -> {
+                checkConfigClass(configClass);
+                Object configInstance;
+                configInstance = tryToCreateConfigInstance(configClass);
+                return getAnnotatedMethodsEntryStream(configClass, configInstance);
+            })
+            .sorted(comparingInt(methodEntry ->
+                methodEntry.getKey().getAnnotation(AppComponent.class).order()))
+            .forEach(entry -> {
+                Method method = entry.getKey();
+                Object configInstance = entry.getValue();
+                String componentName =
+                    method.getAnnotation(AppComponent.class).name();
+                checkDuplicates(componentName);
+                try {
+                    Object[] args = getConstructorParameters(method);
+                    Object componentInstance = method.invoke(configInstance, args);
+                    appComponents.add(componentInstance);
+                    appComponentsByName.put(componentName, componentInstance);
+                } catch (Exception e) {
+                    throw new RuntimeException(String.format("Failed to create component: %s", componentName), e);
+                }
+            });
+    }
+
+    private Stream<Map.Entry<Method, Object>> getAnnotatedMethodsEntryStream(
+        Class<?> configClass, Object configInstance
+    ) {
+        return Arrays.stream(configClass.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(AppComponent.class))
+            .map(method -> Map.entry(method, configInstance));
+    }
+
+    private Object[] getConstructorParameters(Method method) {
+        return Arrays.stream(method.getParameterTypes())
+            .map(this::getAppComponent)
+            .toArray();
+    }
+
+    private void checkDuplicates(String componentName) {
+        if (appComponentsByName.containsKey(componentName)) {
+            throw new IllegalArgumentException(String.format("Duplicate component name: %s", componentName));
+        }
+    }
+
+    private Object tryToCreateConfigInstance(Class<?> configClass) {
+        try {
+            return configClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate config: " + configClass.getName(), e);
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -30,11 +92,25 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        List<C> components = appComponents.stream()
+            .filter(componentClass::isInstance)
+            .map(componentClass::cast)
+            .toList();
+        if (components.isEmpty()) {
+            throw new RuntimeException(String.format("Component not found: %s", componentClass));
+        }
+        if (components.size() > 1) {
+            throw new RuntimeException(String.format("Multiple components found for class: %s", componentClass));
+        }
+        return components.getFirst();
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        Object component = appComponentsByName.get(componentName);
+        if (component == null) {
+            throw new RuntimeException(String.format("Component not found: %s", componentName));
+        }
+        return (C) component;
     }
 }
