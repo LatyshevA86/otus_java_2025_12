2ab17d210eee2ac5d4e77d3d2298ffcd78244151
Латышев Алексей Валерьевич <avlatys3@mts.ru>
2026-05-31 16:21:05 +0300

    add L33-34

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -52,6 +52,14 @@ include("L31-executors")
 include("L32-concurrentCollections:ConcurrentCollections")
 include("L32-concurrentCollections:QueueDemo")
 
+include ("L33-virtualThreads:base")
+include ("L33-virtualThreads:springBoot")
+
+include ("L34-multiprocess:processes-demo")
+include ("L34-multiprocess:sockets-demo")
+include ("L34-multiprocess:rmi-demo")
+include ("L34-multiprocess:grpc-demo")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
